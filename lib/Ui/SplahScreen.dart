b854df826d308adab30c0b2fbda6c8042c38c4a7
nader <nadersalah20201111@gmail.com>
2021-01-25 03:37:56 +0200

    splash & onBoarding added

---
+++ lib/Ui/SplahScreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:footballapp/Ui/OnBoarding.dart';
+import 'package:footballapp/utils/colors_file.dart';
+import 'dart:async';
+
+import 'package:footballapp/utils/navigator.dart';
+
+
+
+// ignore: camel_case_types
+class SplashScreen extends StatefulWidget {
+  @override
+  _openState createState() => _openState();
+}
+
+// ignore: camel_case_types
+class _openState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      navigateAndClearStack(context,OnBoarding());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryAppColor,
+      body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("images/football-player-setting-ball.svg",color: blackColor,height: 100,),
+              SizedBox(height: 20),
+              Text("FootBall",style: TextStyle(color:blackColor,fontSize: 40 ),),
+            ],
+          )
+      ),
+    );
+  }
+}
