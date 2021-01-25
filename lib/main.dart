7af0632b412e46333414d4a873f72e088378b72e
mohamed elshafey <mohamed.elshafey7777@gmail.com>
2021-01-25 03:39:33 +0200

    home screen1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:footballapp/Ui/Screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 }
 
